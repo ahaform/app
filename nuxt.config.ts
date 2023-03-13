4c94b4097f9c3d388aa455bbc301dd84f06f8b52
shanpeng.zhang <shanpeng.zhang@ptmind.com>
2023-03-13 21:19:25 +0800

    fix:添加s3上传

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,5 +5,6 @@ export default defineNuxtConfig({
         bucket:'',
         region:'',
         secret_access_key:'',
+        prefixPath:'/js/form'
     }
 })
---
+++ server/api/upload.ts
@@ -0,0 +1,86 @@
+import AWS from "aws-sdk";
+
+export default defineEventHandler(async (event) => {
+  const body = await readBody(event);
+
+  const { path, data } = body;
+
+  if (!path) {
+    throw createError({
+      statusCode: 400,
+      statusMessage: "path should should be string and not empty",
+    });
+  }
+
+  const template = getJSTemplate(data);
+
+  const { bucket, prefixPath, ...config } = useRuntimeConfig();
+
+  const s3client = getS3Client(config);
+
+  const s3params = buildParams(bucket, `${prefixPath}/${path}`, template);
+
+  const uploadResult = await upload(s3client, s3params);
+
+  return {
+    result: uploadResult,
+  };
+});
+
+function upload(s3client: AWS.S3, params: any) {
+  return new Promise((resolve, reject) => {
+    s3client.upload(params, (err: Error, result: any) => {
+      if (!err) {
+        resolve(result);
+      } else {
+        reject(err);
+      }
+    });
+  });
+}
+
+function buildParams(bucket: string, key: string, value: Buffer | string) {
+  return {
+    Bucket: bucket,
+    Key: key,
+    Body: value,
+    ContentEncoding: "string",
+    ACL: "public-read",
+    CacheControl: "max-age=300",
+    ContentType: "application/javascript",
+  };
+}
+
+function getS3Client(config: {
+  region: string;
+  accessKeyId: string;
+  secret_access_key: string;
+}) {
+  // 上传 aws
+  AWS.config.region = config.region;
+  AWS.config.update({
+    apiVersion: "2006-03-01",
+    accessKeyId: config.accessKeyId,
+    secretAccessKey: config.secret_access_key,
+    region: config.region,
+  });
+  return new AWS.S3();
+}
+
+function getJSTemplate(data: any) {
+  return `
+
+    //TODO:动态请求js基础文件,load完成后初始化
+
+    const template = new AhaFormTemplate.survey({
+        data: ${JSON.stringify(data)}
+    })
+    new AhaForm('#ahaRoot', {
+        template,
+        submitResolve: function (res) {
+            //可以动态注入脚本来处理,通过配置来处理
+            console.log('submit data --->', res)
+        }
+    }).init();
+    `;
+}
