6d97cf433c708c272fa11c0c153bf6b1709b7439
shanpeng.zhang <shanpeng.zhang@ptmind.com>
2023-03-13 18:05:39 +0800

    fix:test env config

--- server/api/hello.ts
+++ server/api/hello.ts
@@ -1,5 +1,7 @@
 export default defineEventHandler((event) => {
+  const { region, bucket } = useRuntimeConfig();
+
   return {
-    msg: "Hello from server",
+    msg: `Hello from server  ${region} , ${bucket}`,
   };
 });
